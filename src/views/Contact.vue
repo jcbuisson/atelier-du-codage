f4416a4630f43be8cbcf3da11844f91b627755f2
JC BUISSON <buisson@enseeiht.fr>
2025-06-02 09:51:32 +0200

    WIP

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,12 +4,13 @@
       <main class="p-16 max-md:p-12 max-sm:p-8 bg-white z-10 rounded-xl flex flex-col gap-6 max-md:gap-4">
 
          <h5>
-            L'Atelier du Codage SAS
+            La Compagnie des Codeurs SAS
          </h5>
 
          <h6>
             36 chemin du Ramelet Moundi<br/>
             31100 TOULOUSE<br/>
+            SIRET 424 929 350 00023<br/>
             06 50 90 04 72
          </h6>
 
